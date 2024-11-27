2a8502f9049213e45e2a04a5cd8ff6fd52427b6c
fechan <56131910+fechan@users.noreply.github.com>
2024-11-27 02:59:29 -0800

    automatically update next meeting time

---
+++ assets/js/meeting-times.js
@@ -0,0 +1,15 @@
+(function () {
+  const DateTime = luxon.DateTime;
+  const Duration = luxon.Duration;
+
+  const meetingInterval = Duration.fromObject({weeks:2});
+  const meetingEpoch = DateTime.fromISO('2024-10-27T15:00:00', {zone: 'America/Los_Angeles'});
+  const now = DateTime.now();
+
+  let nextMeetingDate = meetingEpoch;
+  while (now > nextMeetingDate) {
+    nextMeetingDate = nextMeetingDate.plus(meetingInterval);
+  }
+
+  document.getElementById('meeting-date').textContent = nextMeetingDate.toLocaleString({ weekday: 'long', month: 'long', day: 'numeric' });
+})();
